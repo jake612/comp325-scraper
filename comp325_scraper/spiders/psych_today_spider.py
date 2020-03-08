3fdef4e8fc2737fac160f662b27f50c244914de1
jake612 <jake612@live.unc.edu>
2020-03-08 18:23:03 -0400

    Getting the name of the therapist from their individual page

--- comp325_scraper/spiders/psych_today_spider.py
+++ comp325_scraper/spiders/psych_today_spider.py
@@ -3,11 +3,17 @@ import scrapy
 
 class PsychTodaySpider(scrapy.Spider):
     name = "psychtoday"
+    headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:48.0) Gecko/20100101 Firefox/48.0'}
 
     def start_requests(self):
-        headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:48.0) Gecko/20100101 Firefox/48.0'}
-        return [scrapy.Request(url="https://www.psychologytoday.com/us/therapists/nc/chapel-hill", callback=self.parse, headers=headers)]
+        return [scrapy.Request(url="https://www.psychologytoday.com/us/therapists/nc/chapel-hill", callback=self.parse, headers=self.headers)]
 
     def parse(self, response):
-        for name in response.xpath('//a[@class="result-name"]/@href').getall():
-            self.log(name)
+        # iterates over a list of all the links to therapist page
+        therapist_pages = response.xpath('//a[@class="result-name"]')
+
+        yield from response.follow_all(therapist_pages, headers=self.headers, callback=self.parse_therapist)
+
+    def parse_therapist(self, response):
+        yield {'name': response.xpath('//h1[@itemprop="name"]/text()').get().strip()}
+
